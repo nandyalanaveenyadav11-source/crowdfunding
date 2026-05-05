212ed02a41922075a207cf0b0860a64eb2bbf2ef
Naveen <nandyalanaveenyadav11@gmail.com>
2026-05-05 14:06:32 +0530

    Move mailer registration to register method and clear cache in build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register Custom Brevo API Mail Driver
+        \Illuminate\Support\Facades\Mail::extend('brevo-api', function (array $config) {
+            return new \App\Mail\BrevoTransport(config('services.brevo.key'));
+        });
     }
 
     /**
@@ -20,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Register Custom Brevo API Mail Driver
-        \Illuminate\Support\Facades\Mail::extend('brevo-api', function (array $config) {
-            return new \App\Mail\BrevoTransport(config('services.brevo.key'));
-        });
-
         \Illuminate\Auth\Notifications\VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
             return (new \Illuminate\Notifications\Messages\MailMessage)
                 ->subject('Verify Your CrowdFund Account')
